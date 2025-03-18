ccc61ed9522dbe06620377f6ab4a0acfc36b66b9
Bozhidar Petrov Valchev <61434659+BossyFi@users.noreply.github.com>
2025-03-18 12:28:37 +0100

    [ErrorWeb] Remove Mongo
    Trying to see where the problem is

--- index.js
+++ index.js
@@ -42,26 +42,35 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    console.log(request.body)
-    Person.find({}).then(notes => {
-        response.json(notes)
-    })
+    // Person.find({}).then(notes => {
+    //     response.json(notes)
+    // })
+    response.json(notes)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
+    // const id = request.params.id
+    //
+    // Person.findById(id)
+    //     .then(person => {
+    //         if (person) {
+    //             response.json(person)
+    //         } else {
+    //             response.status(404).send('Person not found, try another ID').end()
+    //         }
+    //     })
+    //     .catch(error => {
+    //         response.status(400).send('Malformatted ID').end()
+    //     })
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
 
-    Person.findById(id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).send('Person not found, try another ID').end()
-            }
-        })
-        .catch(error => {
-            response.status(400).send('Malformatted ID').end()
-        })
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404)
+        response.send('Person not found, try another ID').end()
+    }
 })
 
 const generateId = () => {
@@ -76,53 +85,87 @@ const generateRandomId = () => {
 }
 
 app.post('/api/persons', (request, response) => {
-    const body = request.body;
+    // const body = request.body;
+    //
+    // if (!body.name || !body.number) {
+    //     return response.status(400).json({error: 'content missing'});
+    // }
+    //
+    // Person.findOne({name: body.name}).then(existingPerson => {
+    //     if (existingPerson) {
+    //         return response.status(400).json({error: 'name must be unique'});
+    //     }
+    //
+    //     const person = new Person({
+    //         name: body.name,
+    //         number: body.number,
+    //     });
+    //
+    //     person.save()
+    //         .then(savedPerson => response.json(savedPerson))
+    //         .catch(error => response.status(500).json({error: 'failed to save person'}));
+    // }).catch(error => response.status(500).json({error: 'database error'}));
 
+    const body = request.body
     if (!body.name || !body.number) {
-        return response.status(400).json({error: 'content missing'});
+        return response.status(400).json({
+            error: 'content missing'
+        })
     }
 
-    Person.findOne({name: body.name}).then(existingPerson => {
-        if (existingPerson) {
-            return response.status(400).json({error: 'name must be unique'});
-        }
+    const nameExists = notes.find(note => note.name === body.name)
+    if (nameExists) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
 
-        const person = new Person({
-            name: body.name,
-            number: body.number,
-        });
-
-        person.save()
-            .then(savedPerson => response.json(savedPerson))
-            .catch(error => response.status(500).json({error: 'failed to save person'}));
-    }).catch(error => response.status(500).json({error: 'database error'}));
+    const note = {
+        name: body.name,
+        number: body.number,
+        id: generateRandomId()
+    }
+    notes = notes.concat(note)
+    response.json(note)
 });
 
 app.get('/info', (request, response) => {
-    Person.countDocuments({})
-        .then(count => {
-            const date = new Date();
-            response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`);
-        })
-        .catch(error => {
-            response.status(500).send('Database error').end();
-        });
+    // Person.countDocuments({})
+    //     .then(count => {
+    //         const date = new Date();
+    //         response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`);
+    //     })
+    //     .catch(error => {
+    //         response.status(500).send('Database error').end();
+    //     });
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${notes.length} people</p><p>${date}</p>`)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-
-    Person.findByIdAndDelete(id)
-        .then(deletedPerson => {
-            if (deletedPerson) {
-                response.status(204).end();
-            } else {
-                response.status(404).send('Person not found, try another ID').end();
-            }
-        })
-        .catch(error => {
-            response.status(400).send('Malformatted ID').end();
-        });
+    // const id = request.params.id;
+    //
+    // Person.findByIdAndDelete(id)
+    //     .then(deletedPerson => {
+    //         if (deletedPerson) {
+    //             response.status(204).end();
+    //         } else {
+    //             response.status(404).send('Person not found, try another ID').end();
+    //         }
+    //     })
+    //     .catch(error => {
+    //         response.status(400).send('Malformatted ID').end();
+    //     });
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        notes = notes.filter(note => note.id !== id)
+        response.json(notes)
+        response.status(204).end()
+    } else {
+        response.status(404)
+        response.send('Person not found, try another ID').end()
+    }
 })
 
 const PORT = process.env.PORT || 3001;
